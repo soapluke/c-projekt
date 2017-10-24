97e9cd4f8167afbad56c23751f0285d56c4a6411
soapluke <lukaslindgren@gmail.com>
2017-10-24 12:51:07 +0200

    vlaidationstart

---
+++ RssViewer/Logic/Validation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logic
+{
+    public class Validation
+    {
+        public bool CheckTextboxEmpty(TextBox text)
+        {
+            string checktext = text.ToString();
+
+            if (string.IsNullOrWhiteSpace(checktext))
+            {
+                MessageBox.Show("You can't leave any empty text fields.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
